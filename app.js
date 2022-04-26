fd5d1752e2b50b300507c3a11207e07b5edd52ba
Bakulin Gleb <hardevzip@gmail.com>
2022-04-27 02:15:15 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const path = require('path');
+const {v4} = require('uuid');
+const app = express();
+
+
+let CONTACTS = [
+    {id: v4(), name: 'Борис', value: '1234566', marked: false}
+]
+
+app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'client')))
+
+
+app.get('/api/contacts', (req, res) => {
+
+    setTimeout(() => {
+        res.status(200).json(CONTACTS)
+    }, 1000)
+})
+
+app.post('/api/contacts', (req, res) => {
+    const contact = {...req.body, id: v4(), marked: false};
+    CONTACTS.push(contact)
+    res.status(201).json(contact)
+})
+
+app.delete('/api/contacts/:id', (req, res) => {
+    CONTACTS = CONTACTS.filter(contact => contact.id != req.params.id)
+    res.status(200).json({message: 'Контакт удален'});
+})
+
+app.put('/api/contacts/:id', (req, res) => {
+    const idx = CONTACTS.findIndex(c => c.id === req.params.id)
+    CONTACTS[idx] = req.body;
+    res.status(200).json(CONTACTS[idx]);
+})
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'index.html'))
+});
+
+
+app.listen(3000, () => console.log('Server has been started on port 3000'));
+
